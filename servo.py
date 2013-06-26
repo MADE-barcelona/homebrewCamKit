1817c63aca1dd63cda3ebd93d7e6466f55cefa45
geekfreak <geekfreak@gmail.com>
2013-06-26 19:25:05 +0200

    a bit tidier

--- servo.py
+++ servo.py
@@ -1,71 +1,120 @@
-# Servo Control
+####################################################################
+#
+#   Copyright (c) 2013 MADE-BCN
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are met:
+#
+#   1. Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#   The views and conclusions contained in the software and documentation are those
+#   of the authors and should not be interpreted as representing official policies,
+#   either expressed or implied, of MADE-BCN.
+#
+####################################################################
+
 import time
 
+# twisted web server
+# sudo apt-get install python-twisted
 from twisted.web import server, resource
 from twisted.internet import reactor
 
+##################################################
+#   Utility functions to access the PWM mode on
+#   pin 18 of the raspberry pi
+##################################################
 def set(prop, value):
     try:
-	    f = open("/sys/class/rpi-pwm/pwm0/" + prop, 'w')
-	    f.write(value)
-	    print(prop + " value: " + value)
-	    f.close()	
+        f = open("/sys/class/rpi-pwm/pwm0/" + prop, 'w')
+        f.write(value)
+        print(prop + " value: " + value)
+        f.close()
     except:
-	    print("Error writing to: " + prop + " value: " + value)
+        print("Error writing to: " + prop + " value: " + value)
 
 def setServo(angle):
     set("servo", str(angle))
 
+##################################################
+#   Basic camera controller
+##################################################
 class CameraController(resource.Resource):
-    
+
     isLeaf = 1
     position = 0
     percentage = 50
-    
-    body = "<H1>#POS#</H1>"
-    
+
+    body = "<H1>#POS#</H1>" # Worst case scenario!
+
     print "initial position: ", position, type(position)
-    
+
     def render_GET(self, request):
 
-        # fetch page template.
-        # camera.html     
+        ###################################################
+        #   fetch page template.
+        #   camera.html is reloaded with every request
+        #   in an apparent homage to 1998! but allows very
+        #   rapid change/deploy for the web UI.
+        ###################################################
         try:
             with open("camera.html") as myfile:
                 self.body = "".join(line for line in myfile)
             myfile.closed
         except IOError:
             self.body = "<H1>#POS#</H1>oops..."
-        
+
+        ####################################################
+        #   Simple HTTP get API
+        #
+        #   http://HOSTNAME:8080/nnn
+        #       where nnn is a value between 0 and 180
+        #   http://HOSTNAME:8080/LEFT
+        #   http://HOSTNAME:8080/RIGHT
+        #       to tick the motor left or right
+        ####################################################
         newPosition = int(self.position)
-        rawRequestedPosition = request.path                
-               
+        rawRequestedPosition = request.path
+
         if rawRequestedPosition != "/favicon.ico":
-            if rawRequestedPosition == "/LEFT":
-                print "direction LEFT"
-                newPosition = self.position + 1
-            else: 
-                if rawRequestedPosition == "/RIGHT":
-                    print "direction RIGHT"
-                    newPosition = self.position - 1
-                else:
-                    newPosition = int(filter(type(rawRequestedPosition).isdigit, rawRequestedPosition))
-                    print "requested position" , request.path , type(filter(type(rawRequestedPosition).isdigit, rawRequestedPosition))
+
+            if rawRequestedPosition == "/LEFT"
+                newPosition = self.position + 3
+            elif rawRequestedPosition == "/RIGHT":
+                newPosition = self.position - 3
+            else:
+                newPosition = int(filter(type(rawRequestedPosition).isdigit, rawRequestedPosition))
 
         print request.path , type(self.position), type(newPosition)
-        
+
         if newPosition < 0:
             newPosition = 0
-            
-        if newPosition>180:        
+
+        if newPosition>180:
             newPosition=180
-            
+
         self.position = int(newPosition)
-        
+
         self.percentage = 100 - (100 * float(self.position)/float(180))
-        
+
         setServo(self.position)
-        
+
         request.setHeader("content-type", "text/html")
         return self.body.replace("#POS#", str(self.position)).replace("#PCT#", str(self.percentage)).replace("#RPCT#",str(100- self.percentage))
 
